8cbbeb951976f123f43f985a36ffa12d87512d9f
王鹏飞飞 <8033685+wang-peng-fei@user.noreply.gitee.com>
2020-09-25 14:15:38 +0800

    master提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <h2>01.master</h2>
-  <h1>01.01.01.devvvv</h1>
+ <h1>我是dev</h1>
+ <h2>wo00</h2>
   </div>
 </template>
 
